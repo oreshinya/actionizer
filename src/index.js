61676686393d09e3863fbade3f47f071c348d830
shinya takahashi <s.takahashi313@gmail.com>
2016-06-26 20:05:24 +0900

    Change interface

--- src/index.js
+++ src/index.js
@@ -1,16 +1,29 @@
 import EventEmitter from 'eventemitter3';
 
-const EVENT_NAME = 'ACTIONIZER_STORE.NOTIFY';
+
+const PUT_ACTION_EVENT  = 'ACTIONIZER.ACTION.PUT';
+const CALL_ACTION_EVENT = 'ACTIONIZER.ACTION.CALL';
+
+export const put = (nextState) => {
+  return {type: PUT_ACTION_EVENT, nextState};
+};
+
+export const call = (fn, args) => {
+  return {type: CALL_ACTION_EVENT, fn, args};
+};
+
+
+const EMITTER_EVENT = 'ACTIONIZER.EMITTER.NOTIFY';
 
 export const createStore = (initialState) => {
   let state = initialState;
   const emitter = new EventEmitter();
 
   const subscribe = (listener) => {
-    emitter.on(EVENT_NAME, listener);
+    emitter.on(EMITTER_EVENT, listener);
 
     return () => {
-      emitter.removeListener(EVENT_NAME, listener);
+      emitter.removeListener(EMITTER_EVENT, listener);
     };
   };
 
@@ -18,20 +31,43 @@ export const createStore = (initialState) => {
     return state;
   };
 
-  const next = (nextState) => {
-    if (state !== nextState) {
-      state = nextState;
-      emitter.emit(EVENT_NAME, state);
-    }
+  const setState = (nextState) => {
+    if (state === nextState) { return; }
+
+    state = nextState;
+    emitter.emit(EMITTER_EVENT, state);
   };
 
-  const actionize = (action) => {
-    return action(next, getState);
+  const dispatch = (actionGenerator) => {
+
+    const step = (result) => {
+      const { done, value } = result;
+
+      if (done) { return; }
+
+      switch (value.type) {
+        case PUT_ACTION_EVENT:
+          setState(value.nextState);
+          step(actionGenerator.next(state));
+          break;
+        case CALL_ACTION_EVENT:
+          const promise = value.fn(...value.args);
+          promise.then((val) => {
+            step(actionGenerator.next(val));
+          }).catch((e) => {
+            step(actionGenerator.throw(e));
+          });
+          break;
+      }
+    };
+
+    step(actionGenerator.next());
   };
 
   return {
     subscribe,
     getState,
-    actionize
+    dispatch
   };
+
 };
