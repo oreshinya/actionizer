db6c0079ddb3e4636ca893e85d2dc217cf5af766
shinya takahashi <s.takahashi313@gmail.com>
2016-09-19 21:31:46 +0900

    Remove unnecessary semicolon

--- src/commands.js
+++ src/commands.js
@@ -2,20 +2,20 @@ import { CALL, FORK, REDUCE, CANCEL, SELECT } from './CommandTypes';
 
 export function select(selector = (state) => state) {
   return { type: SELECT, selector };
-};
+}
 
 export function reduce(reducer) {
   return { type: REDUCE, reducer };
-};
+}
 
 export function call(fn, ...args) {
   return { type: CALL, fn, args };
-};
+}
 
 export function fork(actionCreator, ...args) {
   return { type: FORK, actionCreator, args };
-};
+}
 
 export function cancel(actionId) {
   return { type: CANCEL, actionId };
-};
+}
